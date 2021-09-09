528e13d006d4e8c0f3ed5a65a63e771d756a10f0
Xriven <353814512@qq.com>
2021-09-09 19:12:10 +0800

    第二次初始化项目

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="tab-bar-item" @click='itemClick'>
+    <div v-if="!isActive">
+      <slot name='item-icon'></slot>
+    </div>
+    <div v-else>
+      <slot name='item-icon-active'></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name='item-text'></slot>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TabBarItem',
+    data() {
+      return {
+      }
+    },
+    props: {
+      path: String,
+      activeColor:{
+        type:String,
+        default:'Red'
+      }
+    },
+    methods: {
+      itemClick() {
+        this.$router.push(this.path).catch(err => { err })
+      }
+    },
+    computed: {
+      isActive() {
+        return this.$route.path.indexOf(this.path) !==-1
+      },
+      activeStyle(){
+        return this.isActive ? {color:this.activeColor}:{}
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    height: 49px;
+    text-align: center;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    height: 24px;
+    width: 24px;
+
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px
+  }
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="recommend-item">
+      <a :href='item.link' alt="">
+        <img :src="item.image">
+      </a>
+      <div>{{item.title}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'RecommendView',
+    data() {
+      return {
+      }
+    },
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    text-align: center;
+    width: 100%;
+
+    padding: 10px 0 20px;
+    border-bottom: 10px solid rgb(240, 240, 233);
+  }
+  .recommend-item img{
+    width: 85%;
+    margin-bottom: 10px;
+  }
+  .recommend-item{
+    flex:1;
+  }
+</style>
